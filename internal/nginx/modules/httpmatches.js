a2ad1313e5d1578f3844987d792fdf6bc8e52943
Kate Osborn <k.osborn@f5.com>
2022-05-19 14:42:08 +0200

    Convert njs test to table-style; Add more edge case tests

--- internal/nginx/modules/httpmatches.js
+++ internal/nginx/modules/httpmatches.js
@@ -10,16 +10,14 @@ function headersMatch(r, headers) {
     const kv = h.split(':');
     if (kv.length !== 2) {
       r.error('invalid header match: ' + h);
-      return false;
+      throw 500;
     }
     let val = r.headersIn[kv[0]];
     if (!val || val.toLowerCase() !== kv[1]) {
       r.warn('header: ' + h + ' does not exist in request');
-      return false;
+      throw 404;
     }
   }
-
-  return true;
 }
 
 function paramsMatch(r, params) {
@@ -27,20 +25,24 @@ function paramsMatch(r, params) {
     let p = params[i];
     // get index of first = in string
     const idx = p.indexOf('=');
+    // throw error if index is invalid
+    // an index of -1 means "=" is not present
+    // an index of 0 means there is no value
+    // an index of length -1 means there is no key
+    if (idx === -1 || (idx === 0) | (idx === p.length - 1)) {
+      r.error('invalid query parameter: ' + p);
+      throw 500;
+    }
+
     // divide string into key value using the index
     let kv = [p.slice(0, idx), p.slice(idx + 1)];
-    if (kv.length !== 2) {
-      r.error('invalid query parameter: ' + p);
-      return false;
-    }
+
     const val = r.args[kv[0]];
     if (!val || val !== kv[1]) {
       r.warn('query parameter ' + p + ' not in request URI');
-      return false;
+      throw 404;
     }
   }
-
-  return true;
 }
 
 function redirect(r) {
@@ -50,7 +52,7 @@ function redirect(r) {
         matchesVariable +
         ' is not defined on the request object',
     );
-    r.return(404);
+    r.return(500);
     return;
   }
 
@@ -62,21 +64,29 @@ function redirect(r) {
     return;
   }
 
-  if (match.headers && !headersMatch(r, match.headers)) {
-    r.return(404);
-    return;
+  if (match.headers) {
+    try {
+      headersMatch(r, match.headers);
+    } catch (e) {
+      r.return(e);
+      return;
+    }
   }
 
-  if (match.params && !paramsMatch(r, match.params)) {
-    r.return(404);
-    return;
+  if (match.params) {
+    try {
+      paramsMatch(r, match.params);
+    } catch (e) {
+      r.return(e);
+      return;
+    }
   }
 
   // If we pass all the above checks then the request satisfies the http match conditions and we need to redirect to the path.
   // Make sure there is a path to redirect traffic to.
   if (!match.redirectPath) {
     r.error('no path defined in http match');
-    r.return(404);
+    r.return(500);
   }
 
   r.internalRedirect(match.redirectPath);
--- internal/nginx/modules/httpmatches_test.js
+++ internal/nginx/modules/httpmatches_test.js
@@ -1,243 +1,284 @@
 import redirect from './httpmatches.js';
+
 let expect = require('chai').expect;
 
 // NGINX HTTP Request Object.
 //See documentation for all properties available: http://nginx.org/en/docs/njs/reference.html
-let r;
+let r = {
+  // Test mocks
+  return(statusCode) {
+    r.testReturned = statusCode;
+  },
+  internalRedirect(redirectPath) {
+    r.testRedirectedTo = redirectPath;
+  },
+  error(msg) {
+    console.log('\terror:', msg);
+  },
+  warn(msg) {
+    console.log('\twarn:', msg);
+  },
+};
+const testHeaderMatches = {
+  headers: ['header1:value1', 'header2:value2', 'header3:value3'],
+  redirectPath: '/headers',
+};
+const testQueryParamMatches = {
+  params: ['Arg1=value1', 'arg2=value2=SOME=other=value', 'arg3===value3&*1(*+'],
+  redirectPath: '/params',
+};
+const testAllMatchTypes = {
+  method: 'GET',
+  headers: ['header1:value1', 'header2:value2'],
+  params: ['Arg1=value1', 'arg2=value2=SOME=other=value'],
+  redirectPath: '/a-match',
+};
 
 describe('redirect', function () {
   beforeEach(function () {
-    r = {
-      method: 'GET',
-      variables: {},
-      args: {},
-      headersIn: {},
-      // properties added for testing
-      testReturned: 0,
-      testRedirectedTo: '',
-    };
+    // reset fields on the test request object
+    r.method = 'GET';
+    r.variables = {};
+    r.args = {};
+    r.headersIn = {};
+    // properties added for testing
+    r.testReturned = 0;
+    r.testRedirectedTo = '';
+  });
 
-    // Test mocks
-    r.return = function (statusCode) {
-      r.testReturned = statusCode;
-    };
-
-    r.internalRedirect = function (redirectPath) {
-      r.testRedirectedTo = redirectPath;
-    };
-
-    r.error = function (msg) {
-      console.log('\terror:', msg);
-    };
-
-    r.warn = function (msg) {
-      console.log('\twarn:', msg);
-    };
-  });
-  it('returns 404 if http_matches variable is not defined', function () {
-    redirect.redirect(r);
-    expect(r.testReturned).to.equal(404);
-  });
-  it('returns 404 if http_matches is empty', function () {
-    r.variables = {
-      http_matches: '{}',
-    };
-    redirect.redirect(r);
-    expect(r.testReturned).to.equal(404);
-  });
-  it('redirects to the redirectPath if no conditions are defined in match', function () {
-    let matches = {
-      redirectPath: 'no-conditions',
-    };
-    r.variables = {
-      http_matches: JSON.stringify(matches),
-    };
-
-    redirect.redirect(r);
-    expect(r.testRedirectedTo).to.equal('no-conditions');
-  });
-  it('returns 405 if method does not match', function () {
-    let matches = {
-      method: 'GET',
-      redirectPath: 'get-location',
-    };
-    r.method = 'POST';
-    r.variables = {
-      http_matches: JSON.stringify(matches),
-    };
-    redirect.redirect(r);
-    expect(r.testReturned).to.equal(405);
-  });
-  it('redirects to match redirectPath if method matches', function () {
-    let matches = {
-      method: 'GET',
-      redirectPath: 'get-location',
-    };
-
-    r.variables = {
-      http_matches: JSON.stringify(matches),
-    };
-    redirect.redirect(r);
-    expect(r.testRedirectedTo).to.equal('get-location');
-    matches.method = 'POST';
-    matches.redirectPath = 'post-location';
-    r.method = 'POST';
-    r.variables.http_matches = JSON.stringify(matches);
-    redirect.redirect(r);
-    expect(r.testRedirectedTo).to.equal('post-location');
-  });
-  describe('header matching', function () {
-    beforeEach(function () {
-      let matches = {
-        headers: ['header1:value1', 'header2:value2', 'header3:value3'],
-        redirectPath: '/headers',
-      };
-      r.variables = {
-        http_matches: JSON.stringify(matches),
-      };
-    });
-    it('returns 404 if no headers exist in request', function () {
-      // r has no headers
-      redirect.redirect(r);
-      expect(r.testReturned).to.equal(404);
-    });
-    it('returns 404 if not all headers exist in request', function () {
-      r.headersIn = {
-        header3: 'value3',
-        header1: 'value1',
-      };
-      redirect.redirect(r);
-      expect(r.testReturned).to.equal(404);
-    });
-    it('returns 404 if all headers exist in request but not all match', function () {
-      // r has all headers but not all values match
-      r.headersIn = {
-        header3: 'value3',
-        header1: 'value1',
-        header2: 'not-value2',
-      };
-      redirect.redirect(r);
-      expect(r.testReturned).to.equal(404);
-    });
-    it('redirects to redirectPath if all headers exist in request and all match', function () {
-      // r has all headers but not all values match
-      r.headersIn = {
-        header3: 'value3',
-        header1: 'value1',
-        header2: 'VALUE2', // header matching is case-insensitive. NGINX will lowercase the header name but not the value.
-      };
-      redirect.redirect(r);
-      expect(r.testRedirectedTo).to.equal('/headers');
-    });
-  });
-  describe('query parameter matching', function () {
-    beforeEach(function () {
-      let matches = {
-        params: ['Arg1=value1', 'arg2=value2=SOME=other=value', 'arg3===value3&*1(*+'],
-        redirectPath: '/params',
-      };
-      r.variables = {
-        http_matches: JSON.stringify(matches),
-      };
-    });
-    it('returns 404 if no args exist in request', function () {
-      // r has no args
-      redirect.redirect(r);
-      expect(r.testReturned).to.equal(404);
-    });
-    it('returns 404 if not all args exist in request', function () {
-      r.args = {
-        Arg1: 'value1',
-        arg2: 'value2=SOME=other=value',
-      };
-      redirect.redirect(r);
-      expect(r.testReturned).to.equal(404);
-    });
-    it('returns 404 if all args exist in request but not all match', function () {
-      // r has all headers but not all values match
-      r.args = {
-        Arg1: 'value1',
-        arg2: 'value2=SOME=other=value',
-        ARg3: '==value3&*1(*+', // query param matching is case sensitive, so this shouldn't match.
-      };
-      redirect.redirect(r);
-      expect(r.testReturned).to.equal(404);
-    });
-    it('redirects to redirectPath if all args exist in request and all match', function () {
-      // r has all headers but not all values match
-      r.args = {
-        Arg1: 'value1',
-        arg2: 'value2=SOME=other=value',
-        arg3: '==value3&*1(*+',
-      };
-      redirect.redirect(r);
-      expect(r.testRedirectedTo).to.equal('/params');
-    });
-  });
-  describe('multi-condition matching', function () {
-    beforeEach(function () {
-      let matches = {
+  const tests = [
+    {
+      name: 'returns 500 if http_matches variable is not defined',
+      expectedError: 500,
+    },
+    {
+      name: 'returns 500 if http_matches is empty',
+      matches: {},
+      expectedError: 500,
+    },
+    {
+      name: 'redirects to the redirectPath if no conditions are defined in match',
+      matches: {
+        redirectPath: 'no-conditions',
+      },
+      expectedPath: 'no-conditions',
+    },
+    {
+      name: 'returns 405 if method does not match',
+      matches: {
         method: 'GET',
-        headers: ['header1:value1', 'header2:value2'],
-        params: ['Arg1=value1', 'arg2=value2=SOME=other=value'],
-        redirectPath: '/a-match',
-      };
-      r.variables = {
-        http_matches: JSON.stringify(matches),
-      };
-    });
-    it('returns 405 if method does not match', function () {
-      // method doesn't match but everything else does
-      r.method = 'POST';
-      r.headersIn = {
-        header1: 'value1',
-        header2: 'value2',
-      };
-      r.args = {
-        Arg1: 'value1',
-        arg2: 'value2=SOME=other=value',
-      };
+        redirectPath: 'get-location',
+      },
+      expectedError: 405,
+      requestModifier: (r) => {
+        r.method = 'POST';
+      },
+    },
+    {
+      name: 'redirects to match redirectPath if method matches (GET)',
+      matches: {
+        method: 'GET',
+        redirectPath: 'get-location',
+      },
+      expectedPath: 'get-location',
+    },
+    {
+      name: 'redirects to match redirectPath if method matches (POST)',
+      matches: {
+        method: 'POST',
+        redirectPath: 'post-location',
+      },
+      expectedPath: 'post-location',
+      requestModifier: (r) => {
+        r.method = 'POST';
+      },
+    },
+    {
+      name: 'returns 404 if no headers exist in request',
+      matches: testHeaderMatches,
+      expectedError: 404,
+    },
+    {
+      name: 'returns 404 if not all headers exist in request',
+      matches: testHeaderMatches,
+      expectedError: 404,
+      requestModifier: (r) => {
+        r.headersIn = {
+          header3: 'value3',
+          header1: 'value1',
+        };
+      },
+    },
+    {
+      name: 'returns 404 if all headers exist in request but not all match',
+      matches: testHeaderMatches,
+      expectedError: 404,
+      requestModifier: (r) => {
+        r.headersIn = {
+          header3: 'value3',
+          header1: 'value1',
+          header2: 'not-value2',
+        };
+      },
+    },
+    {
+      name: 'returns 500 if a header match is malformed',
+      matches: { headers: ['header-without-a-colon'] },
+      expectedError: 500,
+    },
+    {
+      name: 'redirects to redirectPath if all headers exist in request and all match',
+      matches: testHeaderMatches,
+      expectedPath: '/headers',
+      requestModifier: (r) => {
+        r.headersIn = {
+          header3: 'value3',
+          header1: 'value1',
+          header2: 'VALUE2', // header matching is case-insensitive. NGINX will lowercase the header name but not the value.
+        };
+      },
+    },
+    {
+      name: 'returns 404 if no args exist in request',
+      matches: testQueryParamMatches,
+      expectedError: 404,
+    },
+    {
+      name: 'returns 404 if not all args exist in request',
+      matches: testQueryParamMatches,
+      expectedError: 404,
+      requestModifier: (r) => {
+        r.args = {
+          Arg1: 'value1',
+          arg2: 'value2=SOME=other=value',
+        };
+      },
+    },
+    {
+      name: 'returns 404 if all args exist in request but not all match',
+      matches: testQueryParamMatches,
+      expectedError: 404,
+      requestModifier: (r) => {
+        r.args = {
+          Arg1: 'value1',
+          arg2: 'value2=SOME=other=value',
+          ARg3: '==value3&*1(*+', // query param matching is case sensitive, so this shouldn't match.
+        };
+      },
+    },
+    {
+      name: 'returns 500 if param match is malformed (no "=")',
+      matches: {
+        params: ['arg-without-an-equal-sign'],
+      },
+      expectedError: 500,
+    },
+    {
+      name: 'returns 500 if param match is malformed (no key)',
+      matches: {
+        params: ['=arg-without-a-key'],
+      },
+      expectedError: 500,
+    },
+    {
+      name: 'returns 500 if param match is malformed (no value)',
+      matches: {
+        params: ['arg-without-a-value='],
+      },
+      expectedError: 500,
+    },
+    {
+      name: 'redirects to redirectPath if all args exist in request and all match',
+      matches: testQueryParamMatches,
+      expectedPath: '/params',
+      requestModifier: (r) => {
+        r.args = {
+          Arg1: 'value1',
+          arg2: 'value2=SOME=other=value',
+          arg3: '==value3&*1(*+',
+        };
+      },
+    },
+    {
+      name: 'returns 405 if method does not match',
+      matches: testAllMatchTypes,
+      expectedError: 405,
+      requestModifier: (r) => {
+        r.method = 'POST';
+        r.headersIn = {
+          header1: 'value1',
+          header2: 'value2',
+        };
+        r.args = {
+          Arg1: 'value1',
+          arg2: 'value2=SOME=other=value',
+        };
+      },
+    },
+    {
+      name: 'returns 404 headers do not match',
+      matches: testAllMatchTypes,
+      expectedError: 404,
+      requestModifier: (r) => {
+        r.method = 'GET';
+        r.headersIn = {
+          header1: 'value1',
+        };
+        r.args = {
+          Arg1: 'value1',
+          arg2: 'value2=SOME=other=value',
+        };
+      },
+    },
+    {
+      name: 'returns 404 if args do not match',
+      matches: testAllMatchTypes,
+      expectedError: 404,
+      requestModifier: (r) => {
+        r.method = 'GET';
+        r.headersIn = {
+          header1: 'value1',
+          header2: 'value2',
+        };
+        r.args = {
+          arg2: 'value2=SOME=other=value',
+        };
+      },
+    },
+    {
+      name: 'redirects to redirectPath if all conditions match',
+      matches: testAllMatchTypes,
+      expectedPath: '/a-match',
+      requestModifier: (r) => {
+        r.method = 'GET';
+        r.headersIn = {
+          header1: 'value1',
+          header2: 'value2',
+        };
+        r.args = {
+          Arg1: 'value1',
+          arg2: 'value2=SOME=other=value',
+        };
+      },
+    },
+  ];
+
+  tests.forEach((test) => {
+    it(test.name, () => {
+      if (test.requestModifier) {
+        test.requestModifier(r);
+      }
+      if (test.matches) {
+        r.variables = {
+          http_matches: JSON.stringify(test.matches),
+        };
+      }
       redirect.redirect(r);
-      expect(r.testReturned).to.equal(405);
-    });
-    it('returns 404 headers do not match', function () {
-      // method and args match, but headers don't
-      r.method = 'GET';
-      r.headersIn = {
-        header1: 'value1',
-      };
-      r.args = {
-        Arg1: 'value1',
-        arg2: 'value2=SOME=other=value',
-      };
-      redirect.redirect(r);
-      expect(r.testReturned).to.equal(404);
-    });
-    it('returns 404 if args do not match', function () {
-      // method and headers match, but args don't
-      r.method = 'GET';
-      r.headersIn = {
-        header1: 'value1',
-        header2: 'value2',
-      };
-      r.args = {
-        arg2: 'value2=SOME=other=value',
-      };
-      redirect.redirect(r);
-      expect(r.testReturned).to.equal(404);
-    });
-    it('redirects to redirectPath if all conditions match', function () {
-      r.method = 'GET';
-      r.headersIn = {
-        header1: 'value1',
-        header2: 'value2',
-      };
-      r.args = {
-        Arg1: 'value1',
-        arg2: 'value2=SOME=other=value',
-      };
-      redirect.redirect(r);
-      expect(r.testRedirectedTo).to.equal('/a-match');
+      if (test.expectedPath) {
+        expect(r.testRedirectedTo).to.equal(test.expectedPath);
+      } else {
+        expect(r.testReturned).to.equal(test.expectedError);
+      }
     });
   });
 });
