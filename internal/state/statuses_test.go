nginxinc/nginx-gateway-fabric

commit 918d6506483fb42710a227b4ecb35c9dca43ccc5

Fix HTTPRoute section name related bugs (#568)
- Previously, NKG assumed section names in parentRefs of HTTPRoute
were unique, even when they referenced different Gateways. This
commit fixes that. Fixes https://github.com/nginxinc/nginx-kubernetes-gateway/issues/484
- Previously, when NKG reported status for a parentRef with an empty
section name, it would give the ref the name "unattached" in
the HTTPRoute status. This behavior is not Gateway API spec compliant --
the API prescribe to use empty section name in that case. Now NKG
will use empty section name. See the corresponding
FIXME in the code https://github.com/nginxinc/nginx-kubernetes-gateway/blob/b594695bb42a79653eb8217a6dfd00c6d9594d5b/internal/state/statuses.go#L151
Note: the FIXME advice is wrong.
- Previously, in case of multiple parentRefs in an HTTPRoute, when
reporting their statuses in the HTTPRoute status, NKG could change
the order of the refs in the status. This commit restores the order.
See the corresponding FIXME in
the code https://github.com/nginxinc/nginx-kubernetes-gateway/blob/b594695bb42a79653eb8217a6dfd00c6d9594d5b/internal/status/httproute.go#L25

internal/state/graph/httproute.go
@@ -27,10 +27,14 @@ type Rule struct {
 
 // ParentRef describes a reference to a parent in an HTTPRoute.
 type ParentRef struct {
+	// FailedAttachmentCondition describes the failure of the attachment when Attached is false.
+	FailedAttachmentCondition conditions.Condition
 	// Gateway is the NamespacedName of the referenced Gateway
 	Gateway types.NamespacedName
-	// Idx is the index of the reference in the HTTPRoute.
+	// Idx is the index of the corresponding ParentReference in the HTTPRoute.
 	Idx int
+	// Attached indicates if the ParentRef is attached to the Gateway.
+	Attached bool
 }
 
 // Route represents an HTTPRoute.
@@ -40,11 +44,8 @@ type Route struct {
 	// For now, we assume that the source is only HTTPRoute.
 	// Later we can support more types - TLSRoute, TCPRoute and UDPRoute.
 	Source *v1beta1.HTTPRoute
-	// SectionNameRefs is a map of section names to the referenced NKG Gateways
-	SectionNameRefs map[string]ParentRef
-	// UnattachedSectionNameRefs is a subset of SectionNameRefs that includes sections that could not be attached
-	// to the referenced Gateway. For example, because section does not exist in the Gateway.
-	UnattachedSectionNameRefs map[string]conditions.Condition
+	// ParentRefs includes ParentRefs with NKG Gateways only.
+	ParentRefs []ParentRef
 	// Conditions include Conditions for the HTTPRoute.
 	Conditions []conditions.Condition
 	// Rules include Rules for the HTTPRoute. Each Rule[i] corresponds to the ith HTTPRouteRule.
@@ -85,35 +86,6 @@ func (r *Route) GetAllBackendGroups() []BackendGroup {
 	return groups
 }
 
-// GetAllConditionsForSectionName returns all Conditions for the referenced section name.
-// It panics if the section name does not exist.
-func (r *Route) GetAllConditionsForSectionName(name string) []conditions.Condition {
-	if _, exist := r.SectionNameRefs[name]; !exist {
-		panic(fmt.Errorf("section name %q does not exist", name))
-	}
-
-	count := len(r.Conditions)
-
-	unattachedCond, sectionIsUnattached := r.UnattachedSectionNameRefs[name]
-	if sectionIsUnattached {
-		count++
-	}
-
-	if count == 0 {
-		return nil
-	}
-
-	conds := make([]conditions.Condition, 0, count)
-
-	if sectionIsUnattached {
-		conds = append(conds, unattachedCond)
-	}
-
-	conds = append(conds, r.Conditions...)
-
-	return conds
-}
-
 // buildRoutesForGateways builds routes from HTTPRoutes that reference any of the specified Gateways.
 func buildRoutesForGateways(
 	validator validation.HTTPFieldsValidator,
@@ -140,8 +112,14 @@ func buildSectionNameRefs(
 	parentRefs []v1beta1.ParentReference,
 	routeNamespace string,
 	gatewayNsNames []types.NamespacedName,
-) map[string]ParentRef {
-	sectionNameRefs := make(map[string]ParentRef)
+) []ParentRef {
+	sectionNameRefs := make([]ParentRef, 0, len(parentRefs))
+
+	type key struct {
+		gwNsName    types.NamespacedName
+		sectionName string
+	}
+	uniqueSectionsPerGateway := make(map[key]struct{})
 
 	for i, p := range parentRefs {
 		gw, found := findGatewayForParentRef(p, routeNamespace, gatewayNsNames)
@@ -149,20 +127,25 @@ func buildSectionNameRefs(
 			continue
 		}
 
-		// FIXME(pleshakov): SectionNames across multiple Gateways might collide. Fix that.
 		var sectionName string
 		if p.SectionName != nil {
 			sectionName = string(*p.SectionName)
 		}
 
-		if _, exist := sectionNameRefs[sectionName]; exist {
-			panicForBrokenWebhookAssumption(fmt.Errorf("duplicate section name %q", sectionName))
+		k := key{
+			gwNsName:    gw,
+			sectionName: sectionName,
 		}
 
-		sectionNameRefs[sectionName] = ParentRef{
+		if _, exist := uniqueSectionsPerGateway[k]; exist {
+			panicForBrokenWebhookAssumption(fmt.Errorf("duplicate section name %q for Gateway %s", sectionName, gw.String()))
+		}
+		uniqueSectionsPerGateway[k] = struct{}{}
+
+		sectionNameRefs = append(sectionNameRefs, ParentRef{
 			Idx:     i,
 			Gateway: gw,
-		}
+		})
 	}
 
 	return sectionNameRefs
@@ -207,9 +190,8 @@ func buildRoute(
 	}
 
 	r := &Route{
-		Source:                    ghr,
-		SectionNameRefs:           sectionNameRefs,
-		UnattachedSectionNameRefs: map[string]conditions.Condition{},
+		Source:     ghr,
+		ParentRefs: sectionNameRefs,
 	}
 
 	err := validateHostnames(ghr.Spec.Hostnames, field.NewPath("spec").Child("hostnames"))
@@ -297,7 +279,8 @@ func bindRouteToListeners(r *Route, gw *Gateway) {
 		return
 	}
 
-	for name, ref := range r.SectionNameRefs {
+	for i := 0; i < len(r.ParentRefs); i++ {
+		ref := &r.ParentRefs[i]
 		routeRef := r.Source.Spec.ParentRefs[ref.Idx]
 
 		path := field.NewPath("spec").Child("parentRefs").Index(ref.Idx)
@@ -306,13 +289,13 @@ func bindRouteToListeners(r *Route, gw *Gateway) {
 
 		if routeRef.SectionName == nil || *routeRef.SectionName == "" {
 			valErr := field.Required(path.Child("sectionName"), "cannot be empty")
-			r.UnattachedSectionNameRefs[name] = conditions.NewRouteUnsupportedValue(valErr.Error())
+			ref.FailedAttachmentCondition = conditions.NewRouteUnsupportedValue(valErr.Error())
 			continue
 		}
 
 		if routeRef.Port != nil {
 			valErr := field.Forbidden(path.Child("port"), "cannot be set")
-			r.UnattachedSectionNameRefs[name] = conditions.NewRouteUnsupportedValue(valErr.Error())
+			ref.FailedAttachmentCondition = conditions.NewRouteUnsupportedValue(valErr.Error())
 			continue
 		}
 
@@ -321,7 +304,7 @@ func bindRouteToListeners(r *Route, gw *Gateway) {
 		referencesWinningGw := ref.Gateway.Namespace == gw.Source.Namespace && ref.Gateway.Name == gw.Source.Name
 
 		if !referencesWinningGw {
-			r.UnattachedSectionNameRefs[name] = conditions.NewTODO("Gateway is ignored")
+			ref.FailedAttachmentCondition = conditions.NewTODO("Gateway is ignored")
 			continue
 		}
 
@@ -340,25 +323,27 @@ func bindRouteToListeners(r *Route, gw *Gateway) {
 		// - listener 2 for port 80 with hostname *.example.com;
 		// In this case, the Route host foo.example.com should choose listener 1, as it is a more specific match.
 
-		l, exists := gw.Listeners[name]
+		l, exists := gw.Listeners[string(*routeRef.SectionName)]
 		if !exists {
 			// FIXME(pleshakov): Add a proper condition once it is available in the Gateway API.
 			// https://github.com/nginxinc/nginx-kubernetes-gateway/issues/306
-			r.UnattachedSectionNameRefs[name] = conditions.NewTODO("listener is not found")
+			ref.FailedAttachmentCondition = conditions.NewTODO("listener is not found")
 			continue
 		}
 
 		if !l.Valid {
-			r.UnattachedSectionNameRefs[name] = conditions.NewRouteInvalidListener()
+			ref.FailedAttachmentCondition = conditions.NewRouteInvalidListener()
 			continue
 		}
 
 		accepted := findAcceptedHostnames(l.Source.Hostname, r.Source.Spec.Hostnames)
 
 		if len(accepted) == 0 {
-			r.UnattachedSectionNameRefs[name] = conditions.NewRouteNoMatchingListenerHostname()
+			ref.FailedAttachmentCondition = conditions.NewRouteNoMatchingListenerHostname()
 			continue
 		}
+
+		ref.Attached = true
 
 		for _, h := range accepted {
 			l.AcceptedHostnames[h] = struct{}{}

internal/state/statuses.go
@@ -3,6 +3,7 @@ package state
 import (
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/gateway-api/apis/v1beta1"
 
 	"github.com/nginxinc/nginx-kubernetes-gateway/internal/state/conditions"
 	"github.com/nginxinc/nginx-kubernetes-gateway/internal/state/graph"
@@ -48,19 +49,20 @@ type ListenerStatus struct {
 	AttachedRoutes int32
 }
 
-// ParentStatuses holds the statuses of parents where the key is the section name in a parentRef.
-type ParentStatuses map[string]ParentStatus
-
 // HTTPRouteStatus holds the status-related information about an HTTPRoute resource.
 type HTTPRouteStatus struct {
 	// ParentStatuses holds the statuses for parentRefs of the HTTPRoute.
-	ParentStatuses ParentStatuses
+	ParentStatuses []ParentStatus
 	// ObservedGeneration is the generation of the resource that was processed.
 	ObservedGeneration int64
 }
 
 // ParentStatus holds status-related information related to how the HTTPRoute binds to a specific parentRef.
 type ParentStatus struct {
+	// GatewayNsName is the Namespaced name of the Gateway, which the parentRef references.
+	GatewayNsName types.NamespacedName
+	// SectionName is the SectionName of the parentRef.
+	SectionName *v1beta1.SectionName
 	// Conditions is the list of conditions that are relevant to the parentRef.
 	Conditions []conditions.Condition
 }
@@ -102,14 +104,19 @@ func buildStatuses(graph *graph.Graph) Statuses {
 		defaultConds := conditions.NewDefaultListenerConditions()
 
 		for name, l := range graph.Gateway.Listeners {
-			conds := make([]conditions.Condition, 0, len(l.Conditions)+len(defaultConds)+1) // 1 is for missing GC
+			missingGCCondCount := 0
+			if !gcValidAndExist {
+				missingGCCondCount = 1
+			}
+
+			conds := make([]conditions.Condition, 0, len(l.Conditions)+len(defaultConds)+missingGCCondCount)
 
 			// We add default conds first, so that any additional conditions will override them, which is
 			// ensured by DeduplicateConditions.
 			conds = append(conds, defaultConds...)
 			conds = append(conds, l.Conditions...)
 
-			if !gcValidAndExist {
+			if missingGCCondCount == 1 {
 				// FIXME(pleshakov): Figure out appropriate conditions for the cases when:
 				// (1) GatewayClass is invalid.
 				// (2) GatewayClass does not exist.
@@ -135,30 +142,32 @@ func buildStatuses(graph *graph.Graph) Statuses {
 	}
 
 	for nsname, r := range graph.Routes {
-		parentStatuses := make(map[string]ParentStatus)
+		parentStatuses := make([]ParentStatus, 0, len(r.ParentRefs))
 
 		baseConds := buildBaseRouteConditions(gcValidAndExist)
 
-		for ref := range r.SectionNameRefs {
-			conds := r.GetAllConditionsForSectionName(ref)
+		for _, ref := range r.ParentRefs {
+			failedAttachmentCondCount := 0
+			if !ref.Attached {
+				failedAttachmentCondCount = 1
+			}
+			allConds := make([]conditions.Condition, 0, len(r.Conditions)+len(baseConds)+failedAttachmentCondCount)
 
-			allConds := make([]conditions.Condition, 0, len(conds)+len(baseConds))
 			// We add baseConds first, so that any additional conditions will override them, which is
 			// ensured by DeduplicateConditions.
 			allConds = append(allConds, baseConds...)
-			allConds = append(allConds, conds...)
-
-			if ref == "" {
-				// FIXME(pleshakov): Gateway API spec does allow empty section names in the status.
-				// However, NKG doesn't yet support the empty section names.
-				// Once NKG supports them, it will be able to determine which section name the HTTPRoute was
-				// attached to. So we won't need this workaround.
-				ref = "unattached"
+			allConds = append(allConds, r.Conditions...)
+			if failedAttachmentCondCount == 1 {
+				allConds = append(allConds, ref.FailedAttachmentCondition)
 			}
 
-			parentStatuses[ref] = ParentStatus{
-				Conditions: conditions.DeduplicateConditions(allConds),
-			}
+			routeRef := r.Source.Spec.ParentRefs[ref.Idx]
+
+			parentStatuses = append(parentStatuses, ParentStatus{
+				GatewayNsName: ref.Gateway,
+				SectionName:   routeRef.SectionName,
+				Conditions:    conditions.DeduplicateConditions(allConds),
+			})
 		}
 
 		statuses.HTTPRouteStatuses[nsname] = HTTPRouteStatus{

commit cf47234b2459615ab978638d119e26864732defa

Fix status reporting for invalid HTTPRoutes
918d6506483fb42710a227b4ecb35c9dca43ccc5 introduced a bug which
made NKG fail to report status conditions for an invalid HTTPRoute.
It would try to report an empty condition in the status and fail
because of the status CRD validation (example error):

{"level":"error","ts":"2023-04-18T15:37:49Z","logger":"statusUpdater","msg":"Failed to update status","namespace":"default","name":"coffee","kind":"HTTPRoute","error":"HTTPRoute.gateway.networking.k8s.io \"coffee\" is invalid: [status.parents[0].conditions[1].reason: Invalid value: \"\": status.parents[0].conditions[1].reason in body should be at least 1 chars long, status.parents[0].conditions[1].status: Unsupported value: \"\": supported values: \"True\", \"False\", \"Unknown\", status.parents[0].conditions[1].type: Invalid value: \"\": status.parents[0].conditions[1].type in body should match '^([a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*/)?(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])$']","stacktrace":"github.com/nginxinc/nginx-kubernetes-gateway/internal/status.(*updaterImpl).update\n\t/home/runner/work/nginx-kubernetes-gateway/nginx-kubernetes-gateway/internal/status/updater.go:168\ngithub.com/nginxinc/nginx-kubernetes-gateway/internal/status.(*updaterImpl).Update\n\t/home/runner/work/nginx-kubernetes-gateway/nginx-kubernetes-gateway/internal/status/updater.go:128\ngithub.com/nginxinc/nginx-kubernetes-gateway/internal/events.(*EventHandlerImpl).HandleEventBatch\n\t/home/runner/work/nginx-kubernetes-gateway/nginx-kubernetes-gateway/internal/events/handler.go:90\ngithub.com/nginxinc/nginx-kubernetes-gateway/internal/events.(*EventLoop).Start.func1.1\n\t/home/runner/work/nginx-kubernetes-gateway/nginx-kubernetes-gateway/internal/events/loop.go:67"}

This commit fixes that bug.

internal/state/graph/httproute.go
@@ -27,12 +27,20 @@ type Rule struct {
 
 // ParentRef describes a reference to a parent in an HTTPRoute.
 type ParentRef struct {
-	// FailedAttachmentCondition describes the failure of the attachment when Attached is false.
-	FailedAttachmentCondition conditions.Condition
+	// Attachment is the attachment status of the ParentRef. It could be nil. In that case, NGK didn't attempt to
+	// attach because of problems with the Route.
+	Attachment *ParentRefAttachmentStatus
 	// Gateway is the NamespacedName of the referenced Gateway
 	Gateway types.NamespacedName
 	// Idx is the index of the corresponding ParentReference in the HTTPRoute.
 	Idx int
+}
+
+// ParentRefAttachmentStatus describes the attachment status of a ParentRef.
+type ParentRefAttachmentStatus struct {
+	// FailedCondition is the condition that describes why the ParentRef is not attached to the Gateway. It is set
+	// when Attached is false.
+	FailedCondition conditions.Condition
 	// Attached indicates if the ParentRef is attached to the Gateway.
 	Attached bool
 }
@@ -280,7 +288,10 @@ func bindRouteToListeners(r *Route, gw *Gateway) {
 	}
 
 	for i := 0; i < len(r.ParentRefs); i++ {
+		attachment := &ParentRefAttachmentStatus{}
 		ref := &r.ParentRefs[i]
+		ref.Attachment = attachment
+
 		routeRef := r.Source.Spec.ParentRefs[ref.Idx]
 
 		path := field.NewPath("spec").Child("parentRefs").Index(ref.Idx)
@@ -289,13 +300,13 @@ func bindRouteToListeners(r *Route, gw *Gateway) {
 
 		if routeRef.SectionName == nil || *routeRef.SectionName == "" {
 			valErr := field.Required(path.Child("sectionName"), "cannot be empty")
-			ref.FailedAttachmentCondition = conditions.NewRouteUnsupportedValue(valErr.Error())
+			attachment.FailedCondition = conditions.NewRouteUnsupportedValue(valErr.Error())
 			continue
 		}
 
 		if routeRef.Port != nil {
 			valErr := field.Forbidden(path.Child("port"), "cannot be set")
-			ref.FailedAttachmentCondition = conditions.NewRouteUnsupportedValue(valErr.Error())
+			attachment.FailedCondition = conditions.NewRouteUnsupportedValue(valErr.Error())
 			continue
 		}
 
@@ -304,7 +315,7 @@ func bindRouteToListeners(r *Route, gw *Gateway) {
 		referencesWinningGw := ref.Gateway.Namespace == gw.Source.Namespace && ref.Gateway.Name == gw.Source.Name
 
 		if !referencesWinningGw {
-			ref.FailedAttachmentCondition = conditions.NewTODO("Gateway is ignored")
+			attachment.FailedCondition = conditions.NewTODO("Gateway is ignored")
 			continue
 		}
 
@@ -327,23 +338,23 @@ func bindRouteToListeners(r *Route, gw *Gateway) {
 		if !exists {
 			// FIXME(pleshakov): Add a proper condition once it is available in the Gateway API.
 			// https://github.com/nginxinc/nginx-kubernetes-gateway/issues/306
-			ref.FailedAttachmentCondition = conditions.NewTODO("listener is not found")
+			attachment.FailedCondition = conditions.NewTODO("listener is not found")
 			continue
 		}
 
 		if !l.Valid {
-			ref.FailedAttachmentCondition = conditions.NewRouteInvalidListener()
+			attachment.FailedCondition = conditions.NewRouteInvalidListener()
 			continue
 		}
 
 		accepted := findAcceptedHostnames(l.Source.Hostname, r.Source.Spec.Hostnames)
 
 		if len(accepted) == 0 {
-			ref.FailedAttachmentCondition = conditions.NewRouteNoMatchingListenerHostname()
+			attachment.FailedCondition = conditions.NewRouteNoMatchingListenerHostname()
 			continue
 		}
 
-		ref.Attached = true
+		attachment.Attached = true
 
 		for _, h := range accepted {
 			l.AcceptedHostnames[h] = struct{}{}

internal/state/statuses.go
@@ -133,7 +133,7 @@ func buildStatuses(graph *graph.Graph) Statuses {
 
 		for _, ref := range r.ParentRefs {
 			failedAttachmentCondCount := 0
-			if !ref.Attached {
+			if ref.Attachment != nil && !ref.Attachment.Attached {
 				failedAttachmentCondCount = 1
 			}
 			allConds := make([]conditions.Condition, 0, len(r.Conditions)+len(defaultConds)+failedAttachmentCondCount)
@@ -143,7 +143,7 @@ func buildStatuses(graph *graph.Graph) Statuses {
 			allConds = append(allConds, defaultConds...)
 			allConds = append(allConds, r.Conditions...)
 			if failedAttachmentCondCount == 1 {
-				allConds = append(allConds, ref.FailedAttachmentCondition)
+				allConds = append(allConds, ref.Attachment.FailedCondition)
 			}
 
 			routeRef := r.Source.Spec.ParentRefs[ref.Idx]

internal/state/graph/backend_refs_test.go
@@ -311,9 +311,11 @@ func TestAddBackendGroupsToRouteTest(t *testing.T) {
 
 	sectionNameRefs := []ParentRef{
 		{
-			Idx:      0,
-			Gateway:  types.NamespacedName{Namespace: "test", Name: "gateway"},
-			Attached: true,
+			Idx:     0,
+			Gateway: types.NamespacedName{Namespace: "test", Name: "gateway"},
+			Attachment: &ParentRefAttachmentStatus{
+				Attached: true,
+			},
 		},
 	}
 

internal/state/graph/graph_test.go
@@ -180,9 +180,11 @@ func TestBuildGraph(t *testing.T) {
 		Source: hr1,
 		ParentRefs: []ParentRef{
 			{
-				Idx:      0,
-				Gateway:  client.ObjectKeyFromObject(gw1),
-				Attached: true,
+				Idx:     0,
+				Gateway: client.ObjectKeyFromObject(gw1),
+				Attachment: &ParentRefAttachmentStatus{
+					Attached: true,
+				},
 			},
 		},
 		Rules: []Rule{createValidRuleWithBackendGroup(hr1Group)},
@@ -193,9 +195,11 @@ func TestBuildGraph(t *testing.T) {
 		Source: hr3,
 		ParentRefs: []ParentRef{
 			{
-				Idx:      0,
-				Gateway:  client.ObjectKeyFromObject(gw1),
-				Attached: true,
+				Idx:     0,
+				Gateway: client.ObjectKeyFromObject(gw1),
+				Attachment: &ParentRefAttachmentStatus{
+					Attached: true,
+				},
 			},
 		},
 		Rules: []Rule{createValidRuleWithBackendGroup(hr3Group)},

internal/state/graph/httproute_test.go
@@ -763,6 +763,12 @@ func TestBindRouteToListeners(t *testing.T) {
 	}
 	notValidRoute := &Route{
 		Valid: false,
+		ParentRefs: []ParentRef{
+			{
+				Idx:     0,
+				Gateway: client.ObjectKeyFromObject(gw),
+			},
+		},
 	}
 
 	notValidListener := createModifiedListener(func(l *Listener) {
@@ -773,12 +779,11 @@ func TestBindRouteToListeners(t *testing.T) {
 	})
 
 	tests := []struct {
-		route                                  *Route
-		gateway                                *Gateway
-		expectedRouteUnattachedSectionNameRefs map[string]conditions.Condition
-		expectedGatewayListeners               map[string]*Listener
-		name                                   string
-		expectedSectionNameRefs                []ParentRef
+		route                    *Route
+		gateway                  *Gateway
+		expectedGatewayListeners map[string]*Listener
+		name                     string
+		expectedSectionNameRefs  []ParentRef
 	}{
 		{
 			route: createNormalRoute(),
@@ -790,9 +795,11 @@ func TestBindRouteToListeners(t *testing.T) {
 			},
 			expectedSectionNameRefs: []ParentRef{
 				{
-					Idx:      0,
-					Gateway:  client.ObjectKeyFromObject(gw),
-					Attached: true,
+					Idx:     0,
+					Gateway: client.ObjectKeyFromObject(gw),
+					Attachment: &ParentRefAttachmentStatus{
+						Attached: true,
+					},
 				},
 			},
 			expectedGatewayListeners: map[string]*Listener{
@@ -817,12 +824,14 @@ func TestBindRouteToListeners(t *testing.T) {
 			},
 			expectedSectionNameRefs: []ParentRef{
 				{
-					Idx:      0,
-					Gateway:  client.ObjectKeyFromObject(gw),
-					Attached: false,
-					FailedAttachmentCondition: conditions.NewRouteUnsupportedValue(
-						`spec.parentRefs[0].sectionName: Required value: cannot be empty`,
-					),
+					Idx:     0,
+					Gateway: client.ObjectKeyFromObject(gw),
+					Attachment: &ParentRefAttachmentStatus{
+						Attached: false,
+						FailedCondition: conditions.NewRouteUnsupportedValue(
+							`spec.parentRefs[0].sectionName: Required value: cannot be empty`,
+						),
+					},
 				},
 			},
 			expectedGatewayListeners: map[string]*Listener{
@@ -840,12 +849,14 @@ func TestBindRouteToListeners(t *testing.T) {
 			},
 			expectedSectionNameRefs: []ParentRef{
 				{
-					Idx:      0,
-					Gateway:  client.ObjectKeyFromObject(gw),
-					Attached: false,
-					FailedAttachmentCondition: conditions.NewRouteUnsupportedValue(
-						`spec.parentRefs[0].sectionName: Required value: cannot be empty`,
-					),
+					Idx:     0,
+					Gateway: client.ObjectKeyFromObject(gw),
+					Attachment: &ParentRefAttachmentStatus{
+						Attached: false,
+						FailedCondition: conditions.NewRouteUnsupportedValue(
+							`spec.parentRefs[0].sectionName: Required value: cannot be empty`,
+						),
+					},
 				},
 			},
 			expectedGatewayListeners: map[string]*Listener{
@@ -863,12 +874,14 @@ func TestBindRouteToListeners(t *testing.T) {
 			},
 			expectedSectionNameRefs: []ParentRef{
 				{
-					Idx:      0,
-					Gateway:  client.ObjectKeyFromObject(gw),
-					Attached: false,
-					FailedAttachmentCondition: conditions.NewRouteUnsupportedValue(
-						`spec.parentRefs[0].port: Forbidden: cannot be set`,
-					),
+					Idx:     0,
+					Gateway: client.ObjectKeyFromObject(gw),
+					Attachment: &ParentRefAttachmentStatus{
+						Attached: false,
+						FailedCondition: conditions.NewRouteUnsupportedValue(
+							`spec.parentRefs[0].port: Forbidden: cannot be set`,
+						),
+					},
 				},
 			},
 			expectedGatewayListeners: map[string]*Listener{
@@ -886,10 +899,12 @@ func TestBindRouteToListeners(t *testing.T) {
 			},
 			expectedSectionNameRefs: []ParentRef{
 				{
-					Idx:                       0,
-					Gateway:                   client.ObjectKeyFromObject(gw),
-					Attached:                  false,
-					FailedAttachmentCondition: conditions.NewTODO("listener is not found"),
+					Idx:     0,
+					Gateway: client.ObjectKeyFromObject(gw),
+					Attachment: &ParentRefAttachmentStatus{
+						Attached:        false,
+						FailedCondition: conditions.NewTODO("listener is not found"),
+					},
 				},
 			},
 			expectedGatewayListeners: map[string]*Listener{
@@ -907,10 +922,12 @@ func TestBindRouteToListeners(t *testing.T) {
 			},
 			expectedSectionNameRefs: []ParentRef{
 				{
-					Idx:                       0,
-					Gateway:                   client.ObjectKeyFromObject(gw),
-					Attached:                  false,
-					FailedAttachmentCondition: conditions.NewRouteInvalidListener(),
+					Idx:     0,
+					Gateway: client.ObjectKeyFromObject(gw),
+					Attachment: &ParentRefAttachmentStatus{
+						Attached:        false,
+						FailedCondition: conditions.NewRouteInvalidListener(),
+					},
 				},
 			},
 			expectedGatewayListeners: map[string]*Listener{
@@ -928,10 +945,12 @@ func TestBindRouteToListeners(t *testing.T) {
 			},
 			expectedSectionNameRefs: []ParentRef{
 				{
-					Idx:                       0,
-					Gateway:                   client.ObjectKeyFromObject(gw),
-					Attached:                  false,
-					FailedAttachmentCondition: conditions.NewRouteNoMatchingListenerHostname(),
+					Idx:     0,
+					Gateway: client.ObjectKeyFromObject(gw),
+					Attachment: &ParentRefAttachmentStatus{
+						Attached:        false,
+						FailedCondition: conditions.NewRouteNoMatchingListenerHostname(),
+					},
 				},
 			},
 			expectedGatewayListeners: map[string]*Listener{
@@ -949,10 +968,12 @@ func TestBindRouteToListeners(t *testing.T) {
 			},
 			expectedSectionNameRefs: []ParentRef{
 				{
-					Idx:                       0,
-					Gateway:                   ignoredGwNsName,
-					Attached:                  false,
-					FailedAttachmentCondition: conditions.NewTODO("Gateway is ignored"),
+					Idx:     0,
+					Gateway: ignoredGwNsName,
+					Attachment: &ParentRefAttachmentStatus{
+						Attached:        false,
+						FailedCondition: conditions.NewTODO("Gateway is ignored"),
+					},
 				},
 			},
 			expectedGatewayListeners: map[string]*Listener{
@@ -968,7 +989,13 @@ func TestBindRouteToListeners(t *testing.T) {
 					"listener-80-1": createListener(),
 				},
 			},
-			expectedRouteUnattachedSectionNameRefs: map[string]conditions.Condition{},
+			expectedSectionNameRefs: []ParentRef{
+				{
+					Idx:        0,
+					Gateway:    client.ObjectKeyFromObject(gw),
+					Attachment: nil,
+				},
+			},
 			expectedGatewayListeners: map[string]*Listener{
 				"listener-80-1": createListener(),
 			},

internal/state/statuses_test.go
@@ -15,8 +15,12 @@ import (
 )
 
 func TestBuildStatuses(t *testing.T) {
-	invalidCondition := conditions.Condition{
-		Type:   "Test",
+	invalidRouteCondition := conditions.Condition{
+		Type:   "TestInvalidRoute",
+		Status: metav1.ConditionTrue,
+	}
+	invalidAttachmentCondition := conditions.Condition{
+		Type:   "TestInvalidAttachment",
 		Status: metav1.ConditionTrue,
 	}
 
@@ -37,7 +41,8 @@ func TestBuildStatuses(t *testing.T) {
 	}
 
 	routes := map[types.NamespacedName]*graph.Route{
-		{Namespace: "test", Name: "hr-1"}: {
+		{Namespace: "test", Name: "hr-valid"}: {
+			Valid: true,
 			Source: &v1beta1.HTTPRoute{
 				ObjectMeta: metav1.ObjectMeta{
 					Generation: 3,
@@ -57,15 +62,44 @@ func TestBuildStatuses(t *testing.T) {
 			},
 			ParentRefs: []graph.ParentRef{
 				{
-					Idx:      0,
-					Gateway:  client.ObjectKeyFromObject(gw),
-					Attached: true,
+					Idx:     0,
+					Gateway: client.ObjectKeyFromObject(gw),
+					Attachment: &graph.ParentRefAttachmentStatus{
+						Attached: true,
+					},
 				},
 				{
-					Idx:                       1,
-					Gateway:                   client.ObjectKeyFromObject(gw),
-					Attached:                  false,
-					FailedAttachmentCondition: invalidCondition,
+					Idx:     1,
+					Gateway: client.ObjectKeyFromObject(gw),
+					Attachment: &graph.ParentRefAttachmentStatus{
+						Attached:        false,
+						FailedCondition: invalidAttachmentCondition,
+					},
+				},
+			},
+		},
+		{Namespace: "test", Name: "hr-invalid"}: {
+			Valid:      false,
+			Conditions: []conditions.Condition{invalidRouteCondition},
+			Source: &v1beta1.HTTPRoute{
+				ObjectMeta: metav1.ObjectMeta{
+					Generation: 3,
+				},
+				Spec: v1beta1.HTTPRouteSpec{
+					CommonRouteSpec: v1beta1.CommonRouteSpec{
+						ParentRefs: []v1beta1.ParentReference{
+							{
+								SectionName: helpers.GetPointer[v1beta1.SectionName]("listener-80-1"),
+							},
+						},
+					},
+				},
+			},
+			ParentRefs: []graph.ParentRef{
+				{
+					Idx:        0,
+					Gateway:    client.ObjectKeyFromObject(gw),
+					Attachment: nil,
 				},
 			},
 		},
@@ -114,7 +148,7 @@ func TestBuildStatuses(t *testing.T) {
 			{Namespace: "test", Name: "ignored-gateway"}: {ObservedGeneration: 1},
 		},
 		HTTPRouteStatuses: map[types.NamespacedName]HTTPRouteStatus{
-			{Namespace: "test", Name: "hr-1"}: {
+			{Namespace: "test", Name: "hr-valid"}: {
 				ObservedGeneration: 3,
 				ParentStatuses: []ParentStatus{
 					{
@@ -127,7 +161,20 @@ func TestBuildStatuses(t *testing.T) {
 						SectionName:   helpers.GetPointer[v1beta1.SectionName]("listener-80-2"),
 						Conditions: append(
 							conditions.NewDefaultRouteConditions(),
-							invalidCondition,
+							invalidAttachmentCondition,
+						),
+					},
+				},
+			},
+			{Namespace: "test", Name: "hr-invalid"}: {
+				ObservedGeneration: 3,
+				ParentStatuses: []ParentStatus{
+					{
+						GatewayNsName: client.ObjectKeyFromObject(gw),
+						SectionName:   helpers.GetPointer[v1beta1.SectionName]("listener-80-1"),
+						Conditions: append(
+							conditions.NewDefaultRouteConditions(),
+							invalidRouteCondition,
 						),
 					},
 				},