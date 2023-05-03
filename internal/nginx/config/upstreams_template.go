768cbf588ae98f85599ac9220637c62d358aaf94
Kate Osborn <50597707+kate-osborn@users.noreply.github.com>
2023-05-03 12:30:13 -0600

    Set upstream zone size to 512k (#609)
    Sets the upstream zone size for all upstreams to 512k. This will support up to 648 upstream servers.

--- internal/nginx/config/upstreams_template.go
+++ internal/nginx/config/upstreams_template.go
@@ -1,11 +1,12 @@
 package config
 
-// FIXME(kate-osborn): Add upstream zone size for each upstream.
-// This should be dynamically calculated based on the number of upstreams.
+// FIXME(kate-osborn): Dynamically calculate upstream zone size based on the number of upstreams.
+// 512k will support up to 648 upstream servers.
 var upstreamsTemplateText = `
 {{ range $u := . }}
 upstream {{ $u.Name }} {
     random two least_conn;
+    zone {{ $u.Name }} 512k;
     {{ range $server := $u.Servers }} 
     server {{ $server.Address }};
     {{- end }}
