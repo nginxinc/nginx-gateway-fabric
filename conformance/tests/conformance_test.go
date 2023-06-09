f9073640b32b2671021e4352b477abb148927c9d
Venktesh <ve.patel@f5.com>
2023-06-09 17:12:36 +0100

    PR feedback

--- conformance/tests/conformance_test.go
+++ conformance/tests/conformance_test.go
@@ -1,3 +1,5 @@
+//go:build conformance
+
 /*
 Copyright 2022 The Kubernetes Authors.
 
@@ -5,7 +7,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-        http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +15,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 package tests
 
 import (
@@ -37,8 +38,10 @@ func TestConformance(t *testing.T) {
 	client, err := client.New(cfg, client.Options{})
 	g.Expect(err).To(BeNil())
 
-	_ = v1alpha2.AddToScheme(client.Scheme())
-	_ = v1beta1.AddToScheme(client.Scheme())
+	err = v1alpha2.AddToScheme(client.Scheme())
+	g.Expect(err).To(BeNil())
+	err = v1beta1.AddToScheme(client.Scheme())
+	g.Expect(err).To(BeNil())
 
 	t.Logf(`Running conformance tests with %s GatewayClass\n cleanup: %t\n`+
 		`debug: %t\n enable all features: %t \n supported features: [%v]\n exempt features: [%v]`,
