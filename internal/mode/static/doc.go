683b763663f44eb7a1bce447a0002f2f4d8fc527
Kate Osborn <k.osborn@f5.com>
2023-07-10 15:17:06 -0600

    Add period

--- internal/mode/static/doc.go
+++ internal/mode/static/doc.go
@@ -1,5 +1,5 @@
 /*
 Package static contains all the packages that relate to the static-mode implementation of NKG.
-Static-mode configures NGINX in the scope of a single Gateway resource
+Static-mode configures NGINX in the scope of a single Gateway resource.
 */
 package static
