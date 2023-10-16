d1cedecff9b4c48d08a8c940153cdc0f3647b0c8
Ciara Stacke <c.stacke@f5.com>
2023-10-16 10:04:40 +0100

    Change path and rerun

--- tests/dataplane-performance/scripts/wrk-latency.sh
+++ tests/dataplane-performance/scripts/wrk-latency.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo -e "# Results\n"
 
-echo -e "## Test1: Running coffee path based routing\n"
-wrk -t2 -c10 -d30 http://cafe.example.com/coffee --latency
+echo -e "## Test1: Running latte path based routing\n"
+wrk -t2 -c10 -d30 http://cafe.example.com/latte --latency
 echo -e "\n## Test2: Running coffee header based routing\n"
 wrk -t2 -c10 -d30 http://cafe.example.com/coffee -H "version:v2" --latency
 echo -e "\n## Test3: Running coffee query based routing\n"
