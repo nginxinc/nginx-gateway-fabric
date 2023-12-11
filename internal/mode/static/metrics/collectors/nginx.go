29b7d4b4d005ee10c8582d769af34294c360541e
Kate Osborn <k.osborn@f5.com>
2023-12-11 14:18:30 -0700

    Update to 1.0 prom exporter

--- internal/mode/static/metrics/collectors/nginx.go
+++ internal/mode/static/metrics/collectors/nginx.go
@@ -5,33 +5,36 @@ import (
 	"fmt"
 	"net"
 	"net/http"
-	"time"
 
+	"github.com/go-kit/log"
 	"github.com/nginxinc/nginx-plus-go-client/client"
 	prometheusClient "github.com/nginxinc/nginx-prometheus-exporter/client"
 	nginxCollector "github.com/nginxinc/nginx-prometheus-exporter/collector"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/promlog"
 
 	"github.com/nginxinc/nginx-gateway-fabric/internal/mode/static/metrics"
 )
 
 const (
-	nginxStatusSock        = "/var/run/nginx/nginx-status.sock"
-	nginxStatusURI         = "http://config-status/stub_status"
-	nginxPlusAPISock       = "/var/run/nginx/nginx-plus-api.sock"
-	nginxPlusAPIURI        = "http://nginx-plus-api/api"
-	nginxStatusSockTimeout = 10 * time.Second
+	nginxStatusSock  = "/var/run/nginx/nginx-status.sock"
+	nginxStatusURI   = "http://config-status/stub_status"
+	nginxPlusAPISock = "/var/run/nginx/nginx-plus-api.sock"
+	nginxPlusAPIURI  = "http://nginx-plus-api/api"
 )
 
 // NewNginxMetricsCollector creates an NginxCollector which fetches stats from NGINX over a unix socket
 func NewNginxMetricsCollector(constLabels map[string]string) (prometheus.Collector, error) {
 	httpClient := getSocketClient(nginxStatusSock)
 
-	client, err := prometheusClient.NewNginxClient(&httpClient, nginxStatusURI)
+	ngxClient := prometheusClient.NewNginxClient(&httpClient, nginxStatusURI)
+
+	promLogger, err := newPromLogger()
 	if err != nil {
 		return nil, err
 	}
-	return nginxCollector.NewNginxCollector(client, metrics.Namespace, constLabels), nil
+
+	return nginxCollector.NewNginxCollector(ngxClient, metrics.Namespace, constLabels, promLogger), nil
 }
 
 // NewNginxPlusMetricsCollector creates an NginxCollector which fetches stats from NGINX Plus API over a unix socket
@@ -40,8 +43,34 @@ func NewNginxPlusMetricsCollector(constLabels map[string]string) (prometheus.Col
 	if err != nil {
 		return nil, err
 	}
-	variableLabelNames := nginxCollector.VariableLabelNames{}
-	return nginxCollector.NewNginxPlusCollector(plusClient, metrics.Namespace, variableLabelNames, constLabels), nil
+
+	promLogger, err := newPromLogger()
+	if err != nil {
+		return nil, err
+	}
+
+	collector := nginxCollector.NewNginxPlusCollector(
+		plusClient,
+		metrics.Namespace,
+		nginxCollector.VariableLabelNames{},
+		constLabels,
+		promLogger,
+	)
+
+	return collector, nil
+}
+
+// newPromLogger creates a Prometheus logger that implements to go-kit log.Logger that the prometheus exporter requires.
+func newPromLogger() (log.Logger, error) {
+	logFormat := &promlog.AllowedFormat{}
+
+	if err := logFormat.Set("json"); err != nil {
+		return nil, err
+	}
+
+	logConfig := &promlog.Config{Format: logFormat}
+
+	return promlog.New(logConfig), nil
 }
 
 // getSocketClient gets an http.Client with a unix socket transport.
@@ -60,7 +89,7 @@ func createPlusClient() (*client.NginxClient, error) {
 	var err error
 
 	httpClient := getSocketClient(nginxPlusAPISock)
-	plusClient, err = client.NewNginxClient(&httpClient, nginxPlusAPIURI)
+	plusClient, err = client.NewNginxClient(nginxPlusAPIURI, client.WithHTTPClient(&httpClient))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create NginxClient for Plus: %w", err)
 	}
