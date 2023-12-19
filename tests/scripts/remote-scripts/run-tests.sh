90fd874ab93f7eddc739c8dcdd28e9ae0a17af91
Ciara Stacke <c.stacke@f5.com>
2023-12-19 09:20:19 +0000

    Add nginx prefix to vars

--- tests/scripts/remote-scripts/run-tests.sh
+++ tests/scripts/remote-scripts/run-tests.sh
@@ -6,4 +6,4 @@ source ~/vars.env
 
 echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile && . $HOME/.profile
 
-cd nginx-gateway-fabric/tests && make test TAG=${TAG} PREFIX=${PREFIX} GINKGO_LABEL=${GINKGO_LABEL} GINKGO_FLAGS=${GINKGO_FLAGS} PULL_POLICY=Always GW_SERVICE_TYPE=LoadBalancer GW_SVC_GKE_INTERNAL=true
+cd nginx-gateway-fabric/tests && make test TAG=${TAG} PREFIX=${PREFIX} NGINX_PREFIX=${NGINX_PREFIX} GINKGO_LABEL=${GINKGO_LABEL} GINKGO_FLAGS=${GINKGO_FLAGS} PULL_POLICY=Always GW_SERVICE_TYPE=LoadBalancer GW_SVC_GKE_INTERNAL=true
