fec0736d7e9915941f9644a39c394b458af2e152
Kate Osborn <k.osborn@f5.com>
2024-01-04 14:42:29 -0700

    Add tests

--- internal/mode/static/config_updater.go
+++ internal/mode/static/config_updater.go
@@ -2,14 +2,9 @@ package static
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 
-	"github.com/go-kit/log"
 	"github.com/go-logr/logr"
-	"github.com/prometheus/common/promlog"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -19,97 +14,6 @@ import (
 	ngfAPI "github.com/nginxinc/nginx-gateway-fabric/apis/v1alpha1"
 	"github.com/nginxinc/nginx-gateway-fabric/internal/framework/helpers"
 )
-
-// logLevelSetter defines an interface for setting the logging level of a logger.
-type logLevelSetter interface {
-	SetLevel(string) error
-}
-
-// multiLogLevelSetter sets the log level for multiple logLevelSetters.
-type multiLogLevelSetter struct {
-	setters []logLevelSetter
-}
-
-func newMultiLogLevelSetter(setters ...logLevelSetter) multiLogLevelSetter {
-	return multiLogLevelSetter{setters: setters}
-}
-
-func (m multiLogLevelSetter) SetLevel(level string) error {
-	allErrs := make([]error, 0, len(m.setters))
-
-	for _, s := range m.setters {
-		if err := s.SetLevel(level); err != nil {
-			allErrs = append(allErrs, err)
-		}
-	}
-
-	return errors.Join(allErrs...)
-}
-
-// zapLogLevelSetter sets the level for a zap logger.
-type zapLogLevelSetter struct {
-	atomicLevel zap.AtomicLevel
-}
-
-func newZapLogLevelSetter(atomicLevel zap.AtomicLevel) zapLogLevelSetter {
-	return zapLogLevelSetter{
-		atomicLevel: atomicLevel,
-	}
-}
-
-// SetLevel sets the logging level for the zap logger.
-func (z zapLogLevelSetter) SetLevel(level string) error {
-	parsedLevel, err := zapcore.ParseLevel(level)
-	if err != nil {
-		return err
-	}
-	z.atomicLevel.SetLevel(parsedLevel)
-
-	return nil
-}
-
-// Enabled returns true if the given level is at or above the current level.
-func (z zapLogLevelSetter) Enabled(level zapcore.Level) bool {
-	return z.atomicLevel.Enabled(level)
-}
-
-// leveledPrometheusLogger is a leveled prometheus logger.
-// This interface is required because the promlog.NewDynamic returns an unexported type *logger.
-type leveledPrometheusLogger interface {
-	log.Logger
-	SetLevel(level *promlog.AllowedLevel)
-}
-
-type promLogLevelSetter struct {
-	logger leveledPrometheusLogger
-}
-
-func newPromLogLevelSetter(logger leveledPrometheusLogger) promLogLevelSetter {
-	return promLogLevelSetter{logger: logger}
-}
-
-func newLeveledPrometheusLogger() (leveledPrometheusLogger, error) {
-	logFormat := &promlog.AllowedFormat{}
-
-	if err := logFormat.Set("json"); err != nil {
-		return nil, err
-	}
-
-	logConfig := &promlog.Config{Format: logFormat}
-	logger := promlog.NewDynamic(logConfig)
-
-	return logger, nil
-}
-
-func (p promLogLevelSetter) SetLevel(level string) error {
-	al := &promlog.AllowedLevel{}
-	if err := al.Set(level); err != nil {
-		return err
-	}
-
-	p.logger.SetLevel(al)
-	return nil
-}
 
 // updateControlPlane updates the control plane configuration with the given user spec.
 // If any fields are not set within the user spec, the default configuration values are used.
---
+++ internal/mode/static/log_level_setters_test.go
@@ -0,0 +1,73 @@
+package static
+
+import (
+	"errors"
+	"testing"
+
+	. "github.com/onsi/gomega"
+	"go.uber.org/zap"
+
+	"github.com/nginxinc/nginx-gateway-fabric/internal/mode/static/staticfakes"
+)
+
+func TestMultiLogLevelSetter_SetLevel(t *testing.T) {
+	g := NewWithT(t)
+
+	logr1 := &staticfakes.FakeLogLevelSetter{}
+	logr2 := &staticfakes.FakeLogLevelSetter{}
+	logr3 := &staticfakes.FakeLogLevelSetter{}
+
+	multiSetter := newMultiLogLevelSetter(logr1, logr2, logr3)
+	g.Expect(multiSetter.SetLevel("test")).To(Succeed())
+
+	g.Expect(logr1.SetLevelCallCount()).To(Equal(1))
+	g.Expect(logr2.SetLevelCallCount()).To(Equal(1))
+	g.Expect(logr3.SetLevelCallCount()).To(Equal(1))
+
+	// error case
+	err1 := errors.New("error1")
+	err2 := errors.New("error2")
+	err3 := errors.New("error3")
+
+	logr1.SetLevelReturns(err1)
+	logr2.SetLevelReturns(err2)
+	logr3.SetLevelReturns(err3)
+
+	err := multiSetter.SetLevel("test")
+	g.Expect(err).To(HaveOccurred())
+
+	expErr := errors.Join(err1, err2, err3)
+	g.Expect(err).To(MatchError(expErr))
+}
+
+func TestZapLogLevelSetter_SetLevel(t *testing.T) {
+	g := NewWithT(t)
+
+	zapSetter := newZapLogLevelSetter(zap.NewAtomicLevel())
+
+	g.Expect(zapSetter.SetLevel("error")).To(Succeed())
+	g.Expect(zapSetter.Enabled(zap.ErrorLevel)).To(BeTrue())
+
+	g.Expect(zapSetter.SetLevel("info")).To(Succeed())
+	g.Expect(zapSetter.Enabled(zap.InfoLevel)).To(BeTrue())
+
+	g.Expect(zapSetter.SetLevel("debug")).To(Succeed())
+	g.Expect(zapSetter.Enabled(zap.DebugLevel)).To(BeTrue())
+
+	g.Expect(zapSetter.SetLevel("invalid")).ToNot(Succeed())
+}
+
+func TestPromLogLevelSetter_SetLevel(t *testing.T) {
+	g := NewWithT(t)
+
+	logger, err := newLeveledPrometheusLogger()
+	g.Expect(err).ToNot(HaveOccurred())
+
+	promSetter := newPromLogLevelSetter(logger)
+
+	g.Expect(promSetter.SetLevel("error")).To(Succeed())
+	g.Expect(promSetter.SetLevel("info")).To(Succeed())
+	g.Expect(promSetter.SetLevel("debug")).To(Succeed())
+
+	g.Expect(promSetter.SetLevel("invalid")).ToNot(Succeed())
+}
