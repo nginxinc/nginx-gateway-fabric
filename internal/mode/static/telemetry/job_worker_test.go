6caf02317fc205f0e1aea2a57a0ed9aa967b9b5e
Benjamin Jee <b.jee@f5.com>
2024-02-02 15:59:10 -0800

    Change job worker tests to be unit tests

--- internal/mode/static/telemetry/job_worker_test.go
+++ internal/mode/static/telemetry/job_worker_test.go
@@ -2,9 +2,9 @@ package telemetry_test
 
 import (
 	"context"
+	"testing"
 	"time"
 
-	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -12,75 +12,67 @@ import (
 	"github.com/nginxinc/nginx-gateway-fabric/internal/mode/static/telemetry/telemetryfakes"
 )
 
-var _ = Describe("Job Worker", func() {
-	var (
-		exporter        *telemetryfakes.FakeExporter
-		dataCollector   *telemetryfakes.FakeDataCollector
-		healthCollector *telemetryfakes.FakeHealthChecker
-		expData         telemetry.Data
-		worker          func(context.Context)
-		readyChannel    chan struct{}
-	)
-	const timeout = 10 * time.Second
+func TestCreateTelemetryJobWorker(t *testing.T) {
+	g := NewWithT(t)
 
-	BeforeEach(func() {
-		exporter = &telemetryfakes.FakeExporter{}
-		dataCollector = &telemetryfakes.FakeDataCollector{}
-		healthCollector = &telemetryfakes.FakeHealthChecker{}
+	exporter := &telemetryfakes.FakeExporter{}
+	dataCollector := &telemetryfakes.FakeDataCollector{}
+	healthCollector := &telemetryfakes.FakeHealthChecker{}
 
-		readyChannel = make(chan struct{})
-		healthCollector.GetReadyIfClosedChannelReturns(readyChannel)
+	readyChannel := make(chan struct{})
+	healthCollector.GetReadyIfClosedChannelReturns(readyChannel)
 
-		worker = telemetry.CreateTelemetryJobWorker(zap.New(), exporter, dataCollector, healthCollector)
+	worker := telemetry.CreateTelemetryJobWorker(zap.New(), exporter, dataCollector, healthCollector)
 
-		expData = telemetry.Data{
-			ProjectMetadata: telemetry.ProjectMetadata{Name: "NGF", Version: "1.1"},
-			NodeCount:       3,
-			NGFResourceCounts: telemetry.NGFResourceCounts{
-				Gateways:       1,
-				GatewayClasses: 1,
-				HTTPRoutes:     1,
-				Secrets:        1,
-				Services:       1,
-				Endpoints:      1,
-			},
-		}
-		dataCollector.CollectReturns(expData, nil)
-	})
-
-	DescribeTable(
-		"Job worker runs without any errors",
-		func(sleep time.Duration) {
-			ctx, cancel := context.WithTimeout(context.Background(), timeout)
-
-			go func() {
-				time.Sleep(sleep)
-				close(readyChannel)
-			}()
-
-			worker(ctx)
-			_, data := exporter.ExportArgsForCall(0)
-			Expect(data).To(Equal(expData))
-			cancel()
+	expData := telemetry.Data{
+		ProjectMetadata: telemetry.ProjectMetadata{Name: "NGF", Version: "1.1"},
+		NodeCount:       3,
+		NGFResourceCounts: telemetry.NGFResourceCounts{
+			Gateways:       1,
+			GatewayClasses: 1,
+			HTTPRoutes:     1,
+			Secrets:        1,
+			Services:       1,
+			Endpoints:      1,
 		},
-		Entry("Job worker runs with NGF Pod ready", time.Duration(0)),
-		Entry("Job worker runs with extended time waiting for NGF Pod to be ready", 1*time.Second),
-	)
+	}
+	dataCollector.CollectReturns(expData, nil)
 
-	When("job worker context is canceled", func() {
-		It("should gracefully exist", func() {
-			ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	timeout := 10 * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 
-			go func() {
-				worker(ctx)
-			}()
+	go func() {
+		time.Sleep(1 * time.Second)
+		close(readyChannel)
+	}()
 
-			sleep := 500 * time.Millisecond
-			time.Sleep(sleep)
-			cancel()
+	worker(ctx)
+	_, data := exporter.ExportArgsForCall(0)
+	g.Expect(data).To(Equal(expData))
+	cancel()
+}
 
-			Expect(dataCollector.CollectCallCount()).To(BeZero())
-			Expect(exporter.ExportCallCount()).To(BeZero())
-		})
-	})
-})
+func TestCreateTelemetryJobWorker_ContextCanceled(t *testing.T) {
+	g := NewWithT(t)
+
+	exporter := &telemetryfakes.FakeExporter{}
+	dataCollector := &telemetryfakes.FakeDataCollector{}
+	healthCollector := &telemetryfakes.FakeHealthChecker{}
+
+	readyChannel := make(chan struct{})
+	healthCollector.GetReadyIfClosedChannelReturns(readyChannel)
+
+	worker := telemetry.CreateTelemetryJobWorker(zap.New(), exporter, dataCollector, healthCollector)
+
+	timeout := 10 * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+
+	go func() {
+		worker(ctx)
+	}()
+
+	cancel()
+
+	g.Expect(dataCollector.CollectCallCount()).To(BeZero())
+	g.Expect(exporter.ExportCallCount()).To(BeZero())
+}
