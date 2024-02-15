edf2888a582d1efd6bc352a6ccd188dcba1d20b5
salonichf5 <146118978+salonichf5@users.noreply.github.com>
2024-02-15 11:58:19 -0800

    Update internal/mode/static/telemetry/collector_test.go
    Co-authored-by: bjee19 <139261241+bjee19@users.noreply.github.com>

--- internal/mode/static/telemetry/collector_test.go
+++ internal/mode/static/telemetry/collector_test.go
@@ -271,7 +271,7 @@ var _ = Describe("Collector", Ordered, func() {
 
 	Describe("clusterID collector", func() {
 		When("collecting clusterID", func() {
-			It("throws an error when collecting clusterID", func() {
+			It("should error if the kubernetes client errored when getting the namespace", func() {
 				expectedError := errors.New("there was an error getting clusterID")
 				k8sClientReader.GetCalls(mergeGetCallsWithBase(
 					func(_ context.Context, _ types.NamespacedName, object client.Object, option ...client.GetOption) error {
