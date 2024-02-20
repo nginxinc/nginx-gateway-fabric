cd777a25a0d98555b5d8be2b612d1a9ebd238b35
kaihsun <kaihsun@anyscale.com>
2024-02-20 00:02:25 +0000

    Address comments

--- internal/mode/static/nginx/config/servers.go
+++ internal/mode/static/nginx/config/servers.go
@@ -580,7 +580,7 @@ func generateProxySetHeaders(filters *dataplane.HTTPFilters, grpc bool) []http.H
 	headerLen := len(headerFilter.Add) + len(headerFilter.Set) + len(headerFilter.Remove) + len(headers)
 	proxySetHeaders := make([]http.Header, 0, headerLen)
 	if len(headerFilter.Add) > 0 {
-		addHeaders := convertAddHeaders(headerFilter.Add)
+		addHeaders := createHeadersWithVarName(headerFilter.Add)
 		proxySetHeaders = append(proxySetHeaders, addHeaders...)
 	}
 	if len(headerFilter.Set) > 0 {
@@ -629,7 +629,7 @@ func generateResponseHeaders(filters *dataplane.HTTPFilters) http.ResponseHeader
 	}
 }
 
-func convertAddHeaders(headers []dataplane.HTTPHeader) []http.Header {
+func createHeadersWithVarName(headers []dataplane.HTTPHeader) []http.Header {
 	locHeaders := make([]http.Header, 0, len(headers))
 	for _, h := range headers {
 		mapVarName := "${" + generateAddHeaderMapVariableName(h.Name) + "}"
