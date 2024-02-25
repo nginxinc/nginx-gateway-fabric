5cd44a19431c1c595bc8f3be7c6dfac33a42aa83
kaihsun <kaihsun@anyscale.com>
2024-02-25 17:28:57 +0000

    Add support for ResponseHeaderModifier for HTTPRouteRule objects
    Problem: Users want to add, set, and remove response headers.
    
    Solution: Use add_header and proxy_hide_header NGINX directives to support ResponseHeaderModifier.
    
    If the action is set, we configure the proxy_hide_header directive to remove the header if exists and add_header directive with the given value in the HTTPRoute spec.
    If the action is remove, we configure the proxy_hide_header directive to remove the header.
    If the action is add, we simply configure the add_header directive with the given value in the HTTPRoute spec.

--- internal/mode/static/nginx/config/servers.go
+++ internal/mode/static/nginx/config/servers.go
@@ -600,11 +600,7 @@ func generateProxySetHeaders(filters *dataplane.HTTPFilters, grpc bool) []http.H
 
 func generateResponseHeaders(filters *dataplane.HTTPFilters) http.ResponseHeaders {
 	if filters == nil || filters.ResponseHeaderModifiers == nil {
-		return http.ResponseHeaders{
-			Add:    []http.Header{},
-			Set:    []http.Header{},
-			Remove: []string{},
-		}
+		return http.ResponseHeaders{}
 	}
 
 	headerFilter := filters.ResponseHeaderModifiers
