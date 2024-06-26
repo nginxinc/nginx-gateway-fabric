b42508c0cb486d7599be0b03dbbfc1f6a4ae6c05
Sarthak Agrawal <s.agrawal@f5.com>
2024-06-26 14:32:04 -0600

    add socket name test

--- internal/mode/static/nginx/config/sockets_test.go
+++ internal/mode/static/nginx/config/sockets_test.go
@@ -6,9 +6,16 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func TestGetSocketName(t *testing.T) {
+func TestGetSocketNameTLS(t *testing.T) {
 	res := getSocketNameTLS(800, "*.cafe.example.com")
 
 	g := NewGomegaWithT(t)
 	g.Expect(res).To(Equal("unix:/var/run/nginx/*.cafe.example.com800.sock"))
+}
+
+func TestGetSocketNameHTTPS(t *testing.T) {
+	res := getSocketNameHTTPS(800)
+
+	g := NewGomegaWithT(t)
+	g.Expect(res).To(Equal("unix:/var/run/nginx/https800.sock"))
 }
