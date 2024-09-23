37a0b41d1494cfd129e10ea1919edcc1c9ec44ee
Luca Comellini <luca.com@gmail.com>
2024-09-23 16:42:03 -0700

    Unit tests report

--- internal/mode/static/nginx/modules/test/vitest.config.ts
+++ internal/mode/static/nginx/modules/test/vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+	reporters: ['verbose', 'junit'],
+	outputFile: './njs-unit-tests.xml',
     coverage: {
       reporter: ['text', 'json', 'html'],
     },
