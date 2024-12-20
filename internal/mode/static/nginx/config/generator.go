71a332dead98702b2be9790ae27280b22af03d0e
Saylor Berman <s.berman@f5.com>
2024-12-20 13:55:36 -0700

    Remove version config

--- internal/mode/static/nginx/config/generator.go
+++ internal/mode/static/nginx/config/generator.go
@@ -47,9 +47,6 @@ const (
 	// streamConfigFile is the path to the configuration file with Stream configuration.
 	streamConfigFile = streamFolder + "/stream.conf"
 
-	// configVersionFile is the path to the config version configuration file.
-	configVersionFile = httpFolder + "/config-version.conf"
-
 	// httpMatchVarsFile is the path to the http_match pairs configuration file.
 	httpMatchVarsFile = httpFolder + "/matches.json"
 
@@ -59,10 +56,6 @@ const (
 	// mgmtIncludesFile is the path to the file containing the NGINX Plus mgmt config.
 	mgmtIncludesFile = mainIncludesFolder + "/mgmt.conf"
 )
-
-// ConfigFolders is a list of folders where NGINX configuration files are stored.
-// Volumes here also need to be added to our crossplane ephemeral test container.
-var ConfigFolders = []string{httpFolder, secretsFolder, includesFolder, mainIncludesFolder, streamFolder}
 
 // Generator generates NGINX configuration files.
 // This interface is used for testing purposes only.
@@ -198,7 +191,6 @@ func (g GeneratorImpl) getExecuteFuncs(
 		g.executeStreamServers,
 		g.executeStreamUpstreams,
 		executeStreamMaps,
-		executeVersion,
 	}
 }
 
