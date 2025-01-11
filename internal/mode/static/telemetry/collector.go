4a6c6886455be0f4de54373ebc1926ad3e20f353
Luca Comellini <luca.com@gmail.com>
2025-01-10 17:40:15 -0800

    Update from nginxinc to nginx (3) (#3000)
    (cherry picked from commit 497621b162aa1d881bb0dfd4f1708676dbbd540c)

--- internal/mode/static/telemetry/collector.go
+++ internal/mode/static/telemetry/collector.go
@@ -8,7 +8,7 @@ import (
 	"sort"
 	"strings"
 
-	tel "github.com/nginxinc/telemetry-exporter/pkg/telemetry"
+	tel "github.com/nginx/telemetry-exporter/pkg/telemetry"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -39,7 +39,7 @@ type ConfigurationGetter interface {
 
 // Data is telemetry data.
 //
-//go:generate go run -tags generator github.com/nginxinc/telemetry-exporter/cmd/generator -type=Data -scheme -scheme-protocol=NGFProductTelemetry -scheme-df-datatype=ngf-product-telemetry
+//go:generate go run -tags generator github.com/nginx/telemetry-exporter/cmd/generator -type=Data -scheme -scheme-protocol=NGFProductTelemetry -scheme-df-datatype=ngf-product-telemetry
 type Data struct {
 	// ImageSource tells whether the image was built by GitHub or locally (values are 'gha', 'local', or 'unknown')
 	ImageSource string
@@ -66,7 +66,7 @@ type Data struct {
 
 // NGFResourceCounts stores the counts of all relevant resources that NGF processes and generates configuration from.
 //
-//go:generate go run -tags generator github.com/nginxinc/telemetry-exporter/cmd/generator -type=NGFResourceCounts
+//go:generate go run -tags generator github.com/nginx/telemetry-exporter/cmd/generator -type=NGFResourceCounts
 type NGFResourceCounts struct {
 	// GatewayCount is the number of relevant Gateways.
 	GatewayCount int64
